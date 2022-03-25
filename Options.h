efff2c6d376617454cff9a6c4133e6d2e01613cc
raa180003 <87555100+raa180003@users.noreply.github.com>
2022-03-25 13:29:16 -0500

    Add files via upload

---
+++ Options.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <armadillo>
+
+using namespace arma;
+
+struct Options
+{
+   int rho;          // Initial step size
+   double epsPrim;   // Primal constraint tolerance
+   double epsDual;   // Duality gap tolerance
+   int maxIter;      // Maximum number of iterations
+   mat xInit;        // Initial value of matrix X
+   mat zInit;        // Initial value of matrix Z
+   mat yOneInit;     // Initial value of dual matrix Y1
+   mat yTwoInit;     // Initial value of dual matrix Y2
+   int method;       // Method selection option (Most likely unused)
+};
